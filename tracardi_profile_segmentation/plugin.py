1fab24618d78f0de190e5e2791e96007d198abab
risto <risto.kowaczewski@gmail.com>
2021-11-14 01:02:25 +0100

    Profile add/remove segment plugin

--- tracardi_profile_segmentation/plugin.py
+++ tracardi_profile_segmentation/plugin.py
@@ -15,21 +15,31 @@ class ProfileSegmentAction(ActionRunner):
     def __init__(self, **kwargs):
         self.config = validate(kwargs)
 
+    def _update(self, profile, segment, action):
+        action = action.lower()
+        if action == 'add':
+            if segment not in profile.segments:
+                profile.segments.append(segment)
+            self.profile.replace(profile)
+        elif action == 'remove':
+            if segment in profile.segments:
+                profile.segments.remove(segment)
+            self.profile.replace(profile)
+
     async def run(self, payload):
         dot = self._get_dot_accessor(payload)
         condition = Condition()
-        if await condition.evaluate(self.config.condition, dot):
-            profile = Profile(**dot.profile)
-            if self.config.action.lower() == 'add':
-                if self.config.segment not in profile.segments:
-                    profile.segments.append(self.config.segment)
+        profile = Profile(**dot.profile)
+        try:
+            if await condition.evaluate(self.config.condition, dot):
+                self._update(profile, self.config.true_segment, self.config.true_action)
+                return Result(port="true", value=payload)
             else:
-                if self.config.segment in profile.segments:
-                    profile.segments.remove(self.config.segment)
-
-            self.profile.replace(profile)
-
-        return Result(port="payload", value=payload)
+                self._update(profile, self.config.false_segment, self.config.false_action)
+                return Result(port="false", value=payload)
+        except Exception as e:
+            self.console.error(str(e))
+            return Result(port="error", value=payload)
 
 
 def register() -> Plugin:
@@ -39,43 +49,76 @@ def register() -> Plugin:
             module='tracardi_profile_segmentation.plugin',
             className='ProfileSegmentAction',
             inputs=["payload"],
-            outputs=['payload'],
+            outputs=['true', "false", "error"],
             version='0.6.0',
             license="MIT",
             author="Risto Kowaczewski",
             manual="profile_segment",
             init={
-                "segment": "",
-                "action": "add",  # the default action (we have add and remove to choose form)
-                "condition": ""
+                "condition": "",
+                "true_segment": "",
+                "true_action": "add",
+                "false_segment": "",
+                "false_action": "remove",
             },
             form=Form(groups=[
                 FormGroup(
-                    name="Add or remove segment",
+                    name="Segment logic",
                     description="Set the conditions for profile segmentation.",
                     fields=[
-                        FormField(
-                            id="segment",
-                            name="Segment name",
-                            description="Provide segment name. This name will be used to mark a profile. "
-                                        "Please use dashes instead of spaces.",
-                            component=FormComponent(type="text", props={"label": "segment"})
-                        ),
-                        FormField(
-                            id="action",
-                            name="What would you like to do",
-                            description="Please select either to ADD or REMOVE the segment.",
-                            component=FormComponent(type="select", props={"label": "action", "items": {
-                                "add": "Add segment",
-                                "remove": "Remove segment"
-                            }})
-                        ),
                         FormField(
                             id="condition",
                             name="Condition statement",
                             description="Provide condition for segmentation. If the condition is met then the profile "
-                                        "will be segmented to defined segment.",
+                                        "will be segmented to defined segment, if NOT then the other action can be "
+                                        "performed.",
                             component=FormComponent(type="textarea", props={"label": "condition"})
+                        )
+                    ]
+                ),
+                FormGroup(
+                    name="Segment when condition is met (Positive path)",
+                    description="This will be triggered when the segmentation condition IS met.",
+                    fields=[
+                        FormField(
+                            id="true_action",
+                            name="What would you like to do",
+                            description="Please select either to ADD, REMOVE, or do NOTHING.",
+                            component=FormComponent(type="select", props={"label": "action", "items": {
+                                "add": "Add segment",
+                                "remove": "Remove segment",
+                                "none": "Do nothing"
+                            }})
+                        ),
+                        FormField(
+                            id="true_segment",
+                            name="Segment name",
+                            description="Provide segment name. This name will be used to mark a profile. "
+                                        "Please use dashes instead of spaces.",
+                            component=FormComponent(type="text", props={"label": "segment"})
+                        )
+                    ]
+                ),
+                FormGroup(
+                    name="Segment when condition is NOT met  (Negative path)",
+                    description="This will be triggered when the segmentation condition IS NOT met.",
+                    fields=[
+                        FormField(
+                            id="false_action",
+                            name="What would you like to do",
+                            description="Please select either to ADD, REMOVE, or do NOTHING.",
+                            component=FormComponent(type="select", props={"label": "action", "items": {
+                                "add": "Add segment",
+                                "remove": "Remove segment",
+                                "none": "Do nothing"
+                            }})
+                        ),
+                        FormField(
+                            id="false_segment",
+                            name="Segment name",
+                            description="Provide segment name. This name will be used to mark a profile. "
+                                        "Please use dashes instead of spaces.",
+                            component=FormComponent(type="text", props={"label": "segment"})
                         )
                     ]
                 ),
